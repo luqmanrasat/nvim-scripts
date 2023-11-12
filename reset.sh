e7d4a2f3e18aa298c64f211eda086cedb626204d
Luqman Rasat <luqmanrasat@goodpeople.my>
2023-11-13 05:34:50 +0800

    change rm commands to 1 line

--- reset.sh
+++ reset.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
 # Delete existing env
-rm -rv ~/.config/nvim
-rm -rv ~/.cache/nvim
-rm -rv ~/.local/share/nvim
-rm -rv ~/.local/state/nvim
+rm -rv ~/.config/nvim ~/.cache/nvim ~/.local/share/nvim ~/.local/state/nvim
 
 echo "Done"
